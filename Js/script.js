d05b3e6c75795a93fcf9ddb22384e4aa07fd646c
Dipen Khadka <dipenkhadka191@gmail.com>
2023-05-04 21:41:53 +0545

    Mobile Menu Functionality Added and Some Other Changes

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,15 @@ let myloader = document.querySelector(".custom-loader");
 window.addEventListener("load", ()=>{
     myloader.style.display = "none";
 })
+
+
+let mymblmenu = document.querySelector(".mbl__menu");
+let navlinks = document.querySelector(".header__nav");
+
+mymblmenu.addEventListener("click", openmenu);
+
+function openmenu(){
+    mymblmenu.classList.toggle("active");
+    navlinks.classList.toggle("active");
+}
+
