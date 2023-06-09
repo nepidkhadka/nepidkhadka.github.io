8d07c3cc62302dc67c92bda4c57b11977c3bf029
Dipen Khadka <dipenkhadka191@gmail.com>
2023-06-09 20:11:02 +0545

    Added Alert For Send Message Button

--- Js/script.js
+++ Js/script.js
@@ -7,12 +7,17 @@ window.addEventListener("load", ()=>{
 
 let mymblmenu = document.querySelector(".mbl__menu");
 let navlinks = document.querySelector(".header__nav");
+let sendbtn = document.querySelector(".sendmsg");
 
-mymblmenu.addEventListener("click", openmenu);
+mymblmenu.addEventListener("click", openmenu);  
 
 function openmenu(){
     if(screen.width<768){
-    mymblmenu.classList.toggle("active");
-    navlinks.classList.toggle("active");
+        mymblmenu.classList.toggle("active");
+        navlinks.classList.toggle("active");
     }
 }
+
+sendbtn.addEventListener("click", ()=>{
+    alert("Apologies for the inconvenience. The send message feature is currently unavailable as it is undergoing development. Please reach out to me through my official social media accounts for any inquiries or messages. Thank you for your understanding.")
+});               
