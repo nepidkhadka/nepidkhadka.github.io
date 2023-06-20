a00c236e984efb459940fea7cd6baf77308d766f
Dipen Khadka <dipenkhadka191@gmail.com>
2023-06-20 21:17:49 +0545

    Auto CopyRight Date JS

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,10 @@
+//Custom Loader
 let myloader = document.querySelector(".custom-loader");
-
 window.addEventListener("load", ()=>{
     myloader.remove();
 })
 
-
+//Mobile Menu
 let mymblmenu = document.querySelector(".mbl__menu");
 let navlinks = document.querySelector(".header__nav");
 let sendbtn = document.querySelector(".sendmsg");
@@ -21,3 +21,8 @@ function openmenu(){
 sendbtn.addEventListener("click", ()=>{
     alert("Apologies for the inconvenience. The send message feature is currently unavailable as it is undergoing development. Please reach out to me through my official social media accounts for any inquiries or messages. Thank you for your understanding.")
 });               
+
+//Copyright Date
+const mydate = document.querySelector(".footer__text span");
+const myyear = new Date().getFullYear();
+mydate.textContent = myyear;
